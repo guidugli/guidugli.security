a72ed81455227b1f43d85730fef19c47bb50e037
guidugli <30201723+guidugli@users.noreply.github.com>
2020-11-10 03:41:39 +0000

    Added PAM, User, Kernel Config and cron roles

---
+++ roles/user/files/check_forward_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+grep -E -v '^(root|halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "/sbin/nologin" && $7 != "/usr/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
+  if [ ! -d "$dir" ]; then
+    echo "The home directory ($dir) of user $user does not exist."
+  else
+    if [ ! -h "$dir/.forward" -a -f "$dir/.forward" ]; then
+      echo ".forward file $dir/.forward exists"
+    fi
+  fi
+done
+
---
+++ roles/user/files/check_homedir_exists.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "/sbin/nologin" && $7 != "/usr/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read -r user dir; do
+  if [ ! -d "$dir" ]; then
+    echo "The home directory ($dir) of user $user does not exist."
+  fi
+done
+
